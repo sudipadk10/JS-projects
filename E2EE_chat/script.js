02b4daf5fec000edc9833d8048aed329f7ad8482
sudipadk10 <adhikarisudip869@gmail.com>
2025-03-08 21:15:08 +0545

    E2EE UI change

--- E2EE_chat/script.js
+++ E2EE_chat/script.js
@@ -1,16 +1,25 @@
 function displayText(){
-    let message = document.getElementById("textInput").value;
+    let message = document.getElementById("senderInput").value;
     
     if(message != ""){
         let output = document.querySelector("#chat1")
     output.innerHTML += `<div class="message">${message}</div>`;
-    document.getElementById("textInput").value = "";
+    displayReciever(message);
+    document.getElementById("senderInput").value = "";
     }
     console.log(output);
 }
-document.getElementById("textInput").addEventListener("keydown", function (event) {
+document.getElementById("senderInput").addEventListener("keydown", function (event) {
     if (event.key === "Enter" && !event.shiftKey) { // Enter key (without Shift)
         event.preventDefault(); // Prevents new line in input
         displayText(); // Call send function
     }
+    console.log(getElementByClass("message-box"));
 });
+
+function displayReciever(message) {
+   
+        let output = document.querySelector("#chat3")
+    output.innerHTML += `<div class="message2">${message}</div>`;
+    
+}
